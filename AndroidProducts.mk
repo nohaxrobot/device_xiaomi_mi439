d472f1745b283db555e8e48cba3c513d5f755561
nohaxrobot <97295772+nohaxrobot@users.noreply.github.com>
2022-05-03 11:00:29 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,5 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/yapp_mi439.mk
+    $(LOCAL_DIR)/yaap_mi439.mk
 
-COMMON_LUNCH_CHOICES := \
-    yapp_mi439-user \
-    yapp_mi439-userdebug \
-    yapp_mi439-eng
